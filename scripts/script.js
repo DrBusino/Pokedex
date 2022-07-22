92332604319213e4a2d06e8fc700f410e49e4bfb
DrBusino <pedrocasanova.cunha@gmail.com>
2022-07-22 18:02:14 -0300

    inicio do script

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+const screen = document.querySelector("#screen");
+
+const menuSpan = document.querySelector("#menuSpan");
+const menu = document.querySelector(".menu");
+
+
+let menuOpened = false
+
+    screen.addEventListener("click", () => {
+        if(menuOpened){
+    menu.id = "menu";
+    menuOpened = false;
+        }else{
+            console.log(menuOpened)
+        }
+          
+    });
+
+
+
+
+menuSpan.addEventListener("click", ()=>{
+    if(menuOpened){
+         menu.id = "menu";
+         menuOpened = false;
+    }else{
+         menu.id = "openMenu";
+         menuOpened = true;
+         console.log("menu precionado");
+    }
+});
+
+
