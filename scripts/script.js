d2339a475c9efc232321e96f812c19bd0bd28adc
DrBusino <pedrocasanova.cunha@gmail.com>
2022-07-22 21:58:21 -0300

    mudanças esteticas

--- scripts/script.js
+++ scripts/script.js
@@ -3,31 +3,22 @@ const screen = document.querySelector("#screen");
 const menuSpan = document.querySelector("#menuSpan");
 const menu = document.querySelector(".menu");
 
+let menuOpened = false;
 
-let menuOpened = false
-
-    screen.addEventListener("click", () => {
-        if(menuOpened){
+screen.addEventListener("click", () => {
+  if (menuOpened) {
     menu.id = "menu";
     menuOpened = false;
-        }else{
-            console.log(menuOpened)
-        }
-          
-    });
-
-
-
-
-menuSpan.addEventListener("click", ()=>{
-    if(menuOpened){
-         menu.id = "menu";
-         menuOpened = false;
-    }else{
-         menu.id = "openMenu";
-         menuOpened = true;
-         console.log("menu precionado");
-    }
+  }
 });
 
-
+menuSpan.addEventListener("click", () => {
+  if (menuOpened) {
+    menu.id = "menu";
+    menuOpened = false;
+  } else {
+    menu.id = "openMenu";
+    menuOpened = true;
+    
+  }
+});
